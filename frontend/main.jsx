4b215a000bf104c86544d85c9e5ff7509b95727f
Kin Yat <kwong48@horizon.csueastbay.edu>
2019-12-26 00:25:27 -0800

    added Tile for Maze

---
+++ frontend/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Main extends React.Component {
+  render() {
+    return(
+      <>
+        <nav className='nav-bar'>
+          Nav-bar
+        </nav>
+        <div className='board'>
+          Main-board
+        </div>
+      </>
+    )
+  }
+}
+
+export default Main;
---
+++ frontend/maze/tile.js
@@ -0,0 +1,29 @@
+const TYPE =[
+  'blank',
+  'wall',
+  'start',
+  'end'
+]
+class Tile {
+  constructor(pos, parent) {
+    this.pos = pos;
+    // possible type: 
+    this.type = 'blank';
+    this.parent = parent;
+    this.g = false;
+    this.h = false;
+    this.f = false;
+  }
+
+  changeType(type) {
+    if (TYPE.includes(type)) {
+      this.type = type;
+      return true;
+    } else {
+      console.log('invalid type');
+      return false;
+    }
+  }
+}
+
+module.exports = Tile;
