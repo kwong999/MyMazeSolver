7660c11383bbe072f10bb01525442bca97c77646
Kin Yat <kwong48@horizon.csueastbay.edu>
2020-01-26 16:04:29 -0800

    refactor BuildBoard

--- frontend/component/controller/controller_left.jsx
+++ frontend/component/controller/controller_left.jsx
@@ -1,31 +1,16 @@
 import React from 'react';
+import BuildBoard from './build_board';
 
 class ControllerLeft extends React.Component {
   render() {
     return(
       <div className='controller-left'>
-        <form className='build-board'>
-          <p>Dimension</p>
-          <dl>
-            <dt>Row:</dt>
-            <dd><input
-              type='number'
-              value={this.props.dimensionRow}
-              min='1'
-              max='20'
-              onChange={this.props.handleChange('dimensionRow')}
-            /></dd>
-            <dt>Column:</dt>
-            <dd><input
-              type='number'
-              value={this.props.dimensionCol}
-              min='1'
-              max='20'
-              onChange={this.props.handleChange('dimensionCol')}
-            /></dd>
-          </dl>
-          <button type='submit' onClick={this.props.handleBuildBoard}>Build Board</button>
-        </form>
+        <BuildBoard
+          dimensionRow={this.props.dimensionRow}
+          dimensionCol={this.props.dimensionCol}
+          handleChange={this.props.handleChange}
+          handleBuildBoard={this.props.handleBuildBoard}
+        />
         <div className='tile-option'>
           <div>
             <p>Current Tile Type: <span>{this.props.currentTileType}</span></p>
