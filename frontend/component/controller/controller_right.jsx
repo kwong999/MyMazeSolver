2281030becca66b309d06b97896d6374fd150ec3
Kin Yat <kwong48@horizon.csueastbay.edu>
2020-01-26 16:56:17 -0800

    refactor MovementOption

--- frontend/component/controller/controller_right.jsx
+++ frontend/component/controller/controller_right.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import MovementOption from './movement_option';
 
 const SOLVER = [
   'A* Star',
@@ -9,15 +10,11 @@ class ControllerRight extends React.Component {
   render() {
     return(
       <div className='controller-right'>
+        <MovementOption
+          movement={this.props.movement}
+          changeMovement={this.props.changeMovement}
+        />
         <div className='maze-action'>
-          <div>
-            <p>Movement: </p>
-            <button
-              className={`${(this.props.movement === 'All Direction') ? 'all-direction' : ''}`}
-              onClick={this.props.changeMovement}>
-              {this.props.movement}
-            </button>
-          </div>
           <div>
             <p>Display Searched Tile: </p>
             <input
@@ -27,6 +24,8 @@ class ControllerRight extends React.Component {
               onChange={this.props.changeDisplaySearchedTile}
             />
           </div>
+        </div>
+        <div className='maze-action'>
           <div>
             <p>Solver: </p>
             <select value={this.props.solver} onChange={this.props.changeSolver}>
@@ -38,6 +37,8 @@ class ControllerRight extends React.Component {
           <button onClick={() => this.props.solverFull(this.props.movement, this.props.solver)}>Solve It!</button>
           <button onClick={this.props.fullReset}>Full Reset</button>
           <button onClick={this.props.softReset}>Soft Reset</button>
+        </div>
+        <div className='maze-action'>
           <label>
             <p>Label:</p>
             <dl>
